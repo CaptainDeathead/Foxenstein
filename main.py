e2a2236252f6666ca04f6d866133d93cdd2714b9
Joshua Hall <unstableplazma@gmail.com>
2023-10-18 21:21:11 +0800

    ee

--- main.py
+++ main.py
@@ -15,10 +15,30 @@ HALF_FOV = FOV / 2
 
 SCREEN_DIST = HALF_WIDTH / tan(HALF_FOV)
 
+TEXTURE_SIZE = 256
+HALF_TEXTURE_SIZE = TEXTURE_SIZE // 2
+
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Foxenstine")
 
 debug = False
+
+def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
+    texture = pg.image.load(path)
+    texture = pg.transform.scale(texture, res)
+    return texture
+
+def load_wall_textures():
+    return {
+        0: get_texture("textures/red_brick_wall.png"),
+        1: get_texture("textures/grey_brick_wall.png"),
+        2: get_texture("textures/moss_grey_brick_wall.png")
+    }
+
+objects = [pg.rect.Rect(0, 0, WIDTH, 100), pg.rect.Rect(WIDTH-100, 0, 100, HEIGHT), pg.rect.Rect(0, HEIGHT-100, WIDTH, 100), pg.rect.Rect(0, 0, 100, HEIGHT), pg.rect.Rect(200, 200, 100, 100), pg.rect.Rect(400, 600, 100, 25)]
+objectTypes = [0, 0, 0, 0, 1, 1]
+
+textures = load_wall_textures()
 
 class Player:
     def __init__(self, x, y, rot, num_rays):
@@ -35,9 +55,14 @@ class Player:
         new_y = self.y + dy
 
         # Check if the new position is within the screen boundaries or doesn't collide with objects.
-        if 0 < new_x < WIDTH and 0 < new_y < HEIGHT:
+        if 0 < new_x < WIDTH and 0 < new_y < HEIGHT and not self.checkCollision(new_x, new_y):
             self.x = new_x
             self.y = new_y
+
+    def checkCollision(self, x, y):
+        for obj in objects:
+            if x > obj.x and x < obj.x + obj.width and y > obj.y and y < obj.y + obj.height:
+                return True
 
     def drawRay(self, angle, plainMap, objects) -> int:
         x = int(self.x)
@@ -74,7 +99,7 @@ class Player:
         for i in range(int(self.num_rays)):
             #print((i+1)*rayAngle)
             distance, endPoint = self.drawRay((self.rot - self.fov / 2) + (i+1) * rayAngle, plainMap, objects)
-            distance = distance * cos((self.rot - self.fov / 2) + (i+1) * rayAngle - self.rot)
+            distance *= cos(self.rot - (self.rot - self.fov / 2) + (i+1) * rayAngle + pi/2)
             #print((self.rot - self.fov / 2) + (i+1) * rayAngle * 57.2957795 * 21.333333333333)
             y_buff.append((distance, endPoint, (self.rot - self.fov / 2) + (i+1) * rayAngle * 57.2957795 * 13.3 - 180))
 
@@ -83,7 +108,6 @@ class Player:
 def main():
     plainMap = []
     #objects = [pg.rect.Rect(200, 200, 100, 100), pg.rect.Rect(400, 600, 100, 25)]
-    objects = [pg.rect.Rect(0, 0, WIDTH, 100), pg.rect.Rect(WIDTH-100, 0, 100, HEIGHT), pg.rect.Rect(0, HEIGHT-100, WIDTH, 100), pg.rect.Rect(0, 0, 100, HEIGHT), pg.rect.Rect(200, 200, 100, 100), pg.rect.Rect(400, 600, 100, 25)]
     player = Player(600, 300, 180, int(WIDTH)/RESOLUTION_SCALE)
     lastSwitch = time.time()
 
@@ -147,7 +171,8 @@ def main():
                 proj_height = (SCREEN_DIST / (y[0] + 0.01)) * 100
 
                 color = min(255, abs(255 / ((y[0] + 0.01) / 100)))
-                pg.draw.line(screen, (color, color, color), (y[2], HEIGHT/2 - proj_height/2), (y[2], HEIGHT/2 + proj_height/2), RESOLUTION_SCALE)
+                #pg.draw.line(screen, (color, color, color), (y[2], HEIGHT/2 - proj_height/2), (y[2], HEIGHT/2 + proj_height/2), RESOLUTION_SCALE)
+                pg.draw.rect(screen, (color, color, color), (y[2], HEIGHT/2 + proj_height/2, RESOLUTION_SCALE, abs(proj_height)))
 
         pg.display.flip()
 
