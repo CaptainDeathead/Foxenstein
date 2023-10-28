0965be7079d443902e532cc3e8ffd8c99f1987e9
Joshua Hall <unstableplazma@gmail.com>
2023-10-28 19:07:03 +0800

    Adding updater.py (changelog.txt) V0.9.1.1

--- updater.py
+++ updater.py
@@ -2,8 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 print("Checking for updates...")
-r = requests.get("https://github.com/CaptainDeathead/Plazma-Engine")
 
-soup = BeautifulSoup(r.text, 'html.parser')
-version = soup.find("span", {"class": "css-truncate-target"}).text
-print(f"Latest version: {version}")
+# https://raw.githubusercontent.com/CaptainDeathead/Plazma-Engine/main/version.txt
+
+with open("version.txt", "r") as f:
+    __version__ = f.read()
+    f.close()
+
+r = requests.get("https://raw.githubusercontent.com/CaptainDeathead/Plazma-Engine/main/version.txt")
+version = r.text
+print("Current version: " + version)
+print("This version: " + __version__)
+
+if version != __version__:
+    print("Update available!")
+    print("Getting update...")
+    r = requests.get("https://raw.githubusercontent.com/CaptainDeathead/Plazma-Engine/main/changelog.txt")
+    changelog = r.text
+    print(f"Changelog:\n{changelog}")
